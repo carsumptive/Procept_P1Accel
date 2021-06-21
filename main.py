1742f657d3deff0d619d31040c7ff0b6aa37eb5e
Carson Lansdowne <carsonlansdowne@gmail.com>
2021-06-21 12:50:47 -0700

    Finished w/ Turnarounds
    Addes description to code as well

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
+from scipy.interpolate import make_interp_spline
 
 
 def parse_fixture():
 
     #read the important columns from table
     data = pd.read_csv(r"C:\Users\c.lansdowne\Box\My Cloud - c.lansdowne\_Desktop\FH_TracyQI19Fixture.csv", usecols = ['Time', 'ExtR'])
-    print(data.iat[0,1])
 
     #clean data
     data.dropna(subset = ['ExtR'], inplace=True)
@@ -15,20 +17,19 @@ def parse_fixture():
     accel_times = []
     decel_times = []
 
+    #initialize empty radial velocity list
+    ExtRVel = []
+
     #set sensitivity
     #look ahead & tolerance range
-    decel_margin = .15
-    zero_margin = .1
-    lookahead = 3
-    threshold = .1 #ms
+    nominal = 720
+    decel_margin = .35 #what % decrease of nominal (720) counts as starting deceleration
+    accel_margin = .1 #what % decrease of nominal counts as fully accelerated?
+    zero_margin = .17 #what does the filter consider to be the turnaround point?
+    lookahead = 3 #how many data points ahead does the filter sample?
 
     #Zero R and find Velocity list
     initialR = data.iat[0,1]
-    # initialrtol = .15
-    # count = 0 #count the number of measurements in initial R
-
-    ExtRVel = []
-
     i = 0
     for row in data.index:
         r = data['ExtR'][row]
@@ -38,22 +39,32 @@ def parse_fixture():
         Vel = (r-data['ExtR'][row-1])/(data['Time'][row]-data['Time'][row-1])
         ExtRVel.append(Vel)
 
-    print(ExtRVel)
     data['ExtRVel'] = ExtRVel
-    #plt.plot(data['Time'], data['ExtRVel'])
+    data['ExtRVelreg'] = ExtRVel #non abs vel data
     data['ExtRVel'] = data['ExtRVel'].abs()
-    print()
+    # Main_spline = make_interp_spline(data['Time'],data['ExtRVelreg'])
+    # x_ = np.linspace(data['Time'].min(), data['Time'].max(), 1000)
+    # y_ = Main_spline(x_)
+    # plt.plot(x_, y_)
 
-    #plt.plot(data['Time'], data['ExtRVel'])
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(3)
+    #ax1.plot(data['Time'], data['ExtRVel'])
 
-
-    #find decel time
-    #look ahead to check decel range
+    #initialize filter variables
     vel = []
+    turnaround = []
     decel_start = 0
+    accel_start = 0
     decel_end = 0
+    accel_end = 0
 
-    print(len(data['Time']))
+    #filter algorithm
+    ##Takes absolute value data and forward steps the average and checks for deceleration within lookahead span
+    ##if decel is found, decel span is measured and accel span immediately follows
+    ##if acceleration ends, reset measurement and add to turnaround array
+    ##turnaround is summed accel and decel times
+
+    #tune what the filter consideres a decel start, accel end, and zero (decel completed) above
 
     for row in data.index:
         if row + lookahead < len(data['Time']):
@@ -63,44 +74,59 @@ def parse_fixture():
                 vel = []
                 for i in range(lookahead-1):
                     vel.append(data['ExtRVel'][row + i + 1])
-                    if vel[i] <= 720*zero_margin:
+                    if vel[i] <= nominal*zero_margin:
                         isdecel = True
-                    #print(vel)
-                #print("writing vel")
-            # vel[1] = data['ExtR'][row + 2]
-            # vel[2] = data['ExtR'][row + 3]
-            # vel[3] = data['ExtR'][row + 4]
-            # vel[4] = data['ExtR'][row + 5]
             ave_vel = sum(vel)/len(vel)
-            print("average", ave_vel)
+            #print("average", ave_vel)
+            #print("data, ", data['ExtRVel'][row])
             # print("extRvel", data['ExtRVel'][row])
             # print("extRvel1", vel[0])
             # print(isdecel)
-            if ave_vel < 720*(1-decel_margin)  and data['ExtRVel'][row] >= 720*(1-decel_margin) and vel[0] <= 720*(1-decel_margin) and isdecel:
+            if ave_vel < nominal*(1-decel_margin) and data['ExtRVel'][row] >= nominal*(1-decel_margin) and vel[1] <= nominal*(1-decel_margin) and isdecel:
                 decel_start = data['Time'][row]
-                print(data['ExtRVel'][row])
-                print("yes", decel_start)
-                print("no", decel_end)
-            if data['ExtRVel'][row] <= 720*zero_margin and decel_start != 0:
+                #print(data['ExtRVel'][row])
+            if data['ExtRVel'][row] <= nominal*zero_margin and decel_start != 0:
                 decel_end = data['Time'][row]
-                print("decel end", decel_end)
+                accel_start = data['Time'][row]
+                #print("decel end", decel_end)
             if decel_end != 0 and decel_start != 0:
-                print("decel time", decel_end - decel_start)
+                #print("decel time", decel_end - decel_start)
                 decel_times.append(decel_end - decel_start)
-                #print("decel Time", decel_times)
                 decel_end = 0
                 decel_start = 0
+            if accel_start != 0 and data['ExtRVel'][row] >= nominal*(1-accel_margin):
+                accel_end = data['Time'][row]
+                accel_times.append(accel_end - accel_start)
+                turnaround.append(accel_times[-1] + decel_times[-1])
+                #print("accel time", accel_end - accel_start)
+                accel_start = 0
+                decel_end = 0
 
-    print(decel_times)
-    plt.plot(decel_times, 'o')
+    #plot decel times
+    ax1.plot(decel_times, 'o')
     plt.ylabel('Deceleration time (s)')
     plt.xlabel('Turn #')
-    plt.title('Plot of all Deceleration times')
+    ax1.set_title('Plot of all Deceleration times')
+    #plot accel times
+    ax3.plot(accel_times, 'o')
+    plt.ylabel('Acceleration time (s)')
+    plt.xlabel('Turn #')
+    ax3.set_title('Plot of all Acceleration times')
+    #plot accel times
+    ax4.plot(turnaround, 'o')
+    plt.ylabel('Turnaround time (s)')
+    plt.xlabel('Turn #')
+    ax4.set_title('Plot of all Turnaround times')
+
+    #write to csv
+    with open('C:/Users/c.lansdowne/Documents/GitHub/P1Accel/decel_times1.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(["Decel times"])
+        csvwriter.writerow(decel_times)
+        csvwriter.writerow(["Accel times"])
+        csvwriter.writerow(accel_times)
 
     plt.show()
-
-
-    #find accel time
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
